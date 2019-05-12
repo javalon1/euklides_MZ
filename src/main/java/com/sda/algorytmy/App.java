909c8ff409e236e51b515c04939daa431ef87cee
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-12 12:49:54 +0100

    nowe rzeczy

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,26 @@
 package com.sda.algorytmy;
+import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int licznik =0;
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+            licznik++;
+        }
+
+        System.out.println("Liczba iteracji:" +licznik);
+        System.out.println("NWD:" + a);
     }
 }
